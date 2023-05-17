837596e71a9007d85677933dd9840dc78094fb51
ayodhya.teslk@gmail.com <ayodhyaratnayake7001@gmail.com>
2023-05-18 03:42:05 +0530

    initial commit

---
+++ Topics.php
@@ -0,0 +1,28 @@
+<?php // <--- do NOT put anything before this PHP tag
+	include('Functions.php');
+	$cookieMessage = getCookieMessage();
+	$cookieUser = getCookieUser();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles.css"> 
+	<meta charset="UTF-8">		<!-- For emojis -->
+</head>
+<body>
+	<div class="container">
+		<div class="row", id="header">
+			
+		</div>
+		<div class="row", id="nav">  
+			
+		</div>
+		<div class="row", id="content">
+			
+		</div>
+		<div class="row", id="footer">
+			
+		</div>
+	</div>
+</body>
+</html>
