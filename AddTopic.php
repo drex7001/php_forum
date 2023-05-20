c42e69fdfd48d342fad9961c16236740608f6494
ayodhya.teslk@gmail.com <ayodhyaratnayake7001@gmail.com>
2023-05-20 23:13:31 +0530

    topic

--- AddTopic.php
+++ AddTopic.php
@@ -1,30 +1,51 @@
-<?php // <--- do NOT put anything before this PHP tag
-
-// this php file will have no HTML
+<?php
 include('Functions.php');
 
-// Sanitize the form data to prevent SQL injection	
-$topic = makeOutputSafe($_POST['topic']);
+// Check if the data from the POST was provided using 'isset', if not echo an error message (This does not need to be formated) (Note that only the username is passed this tme).
+if (!isset($_POST['topic'])) {
+	echo "Error: No topic was provided.";
+	exit;
+}
+
+// Trim the input from the POST and store in a php variable
+$topic = trim($_POST['topic']);
+$dbh = connectToDatabase();
 
 
-$currentDateTime = date('Y-m-d H:i:s');
+// Prepare, bind and execute the SQL command to check if topic already exists
+$sql = "SELECT * FROM topic WHERE topic = ? COLLATE NOCASE";
+$statement = $dbh->prepare($sql);
+$statement->bindValue(1, $topic);
+$statement->execute();
 
-// Connect to the SQLite database
-$db = connectToDatabase();
+// If the topic already exists then set the cookie message to indicate that the topic already exists and redirect to the Topics.php page
+if ($statement->rowCount() > 0) {
+	setcookie('topic_already_exists', 'true', time() + (3600 * 24));
+	header('Location: Topics.php');
+	exit;
+}
 
-// Prepare the SQL statement
-$stmt = $db->prepare("INSERT INTO Topic (DateTime, Topic) VALUES (:currentDateTime, :topic)");
+// Else
+else {
 
-// Bind the parameters
-$stmt->bindValue(":currentDateTime", $currentDateTime, SQLITE3_TEXT);
-$stmt->bindValue(":topic", $topic, SQLITE3_TEXT);
-// Execute the statement
-$stmt->execute();
+	// Get the id of the user using cookies
+	$statement = $dbh->prepare('SELECT UserID FROM user WHERE username = ?');
+	$statement->bindValue(1, 35);
+	$statement->execute();
+	$row = $statement->fetch(PDO::FETCH_ASSOC);
+	$userID = 35;
 
-// Close the database connection
-$db->close();
+	// Set the datetime to Melbourne time using the following code:
+	date_default_timezone_set("Australia/Melbourne");
 
-// Redirect to a success page or perform any other desired actions
-header("Location: success.php");
-exit();
-?>
+	// Insert the topic into the database and then redirect back to Topics.php
+	$sql = "INSERT INTO topic (UserID, datetime, topic) VALUES (?, ?, ?)";
+	$statement = $dbh->prepare($sql);
+	$statement->bindValue(1, $userID);
+	$statement->bindValue(2, date("Y/m/d H:i:s"));
+	$statement->bindValue(3, $topic);
+	$statement->execute();
+
+	header('Location: Topics.php');
+	exit;
+}
--- Topics.php
+++ Topics.php
@@ -1,21 +1,23 @@
 <?php // <--- do NOT put anything before this PHP tag
-	include('Functions.php');
-	$cookieMessage = getCookieMessage();
-	$cookieUser = getCookieUser();
+include('Functions.php');
+$cookieMessage = getCookieMessage();
+$cookieUser = getCookieUser();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<link rel="stylesheet" type="text/css" href="styles.css"> 
-	<meta charset="UTF-8">		<!-- For emojis -->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<meta charset="UTF-8"> <!-- For emojis -->
 </head>
+
 <body>
 	<div class="container">
-		<div class="row", id="header">
-		<?php @include('Header.php'); ?>
+		<div class="row" , id="header">
+			<?php @include('Header.php'); ?>
 		</div>
-		<div class="row", id="nav">  
-			
+		<div class="row" , id="nav">
+
 		</div>
 		<div class="row" , id="content">
 			<div class="sign_in_container">
@@ -26,9 +28,81 @@
 					<input type="submit" value="Submit">
 				</form>
 			</div>
-		<div class="row", id="footer">	
-		<?php @include('Footer.php'); ?>	
+			<div>
+				<!-- <?php
+				if (isset($_COOKIE['topic_already_exists']) && $_COOKIE['topic_already_exists'] == 'true') {
+					echo "The topic you entered already exists.";
+				} else {
+
+					// Get all of the topics from the database
+					$dbh = connectToDatabase();
+					$sql = "SELECT * FROM topic";
+					$statement = $dbh->prepare($sql);
+					$statement->execute();
+					$topics = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+					// Loop through the topics and display them
+					echo "<table>";
+					echo "<tr><th>Topic</th><th>User ID</th><th>Datetime</th></tr>";
+					foreach ($topics as $topic) {
+						echo "<tr><td>" . $topic['Topic'] . "</td><td>" . $topic['UserID'] . "</td><td>" . $topic['DateTime'] . "</td></tr>";
+					}
+					echo "</table>";
+				}
+				?> -->
+
+				<?php
+				if (isset($_COOKIE['topic_already_exists']) && $_COOKIE['topic_already_exists'] == 'true') {
+					echo "The topic you entered already exists.";
+				} else {
+
+					// Get all of the topics from the database
+					$dbh = connectToDatabase();
+					$sql = "SELECT * FROM topic";
+					$statement = $dbh->prepare($sql);
+					$statement->execute();
+					$topics = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+					// Get the total number of topics
+					$totalTopics = count($topics);
+
+					// Set the default page number to 1
+					$pageNumber = 1;
+
+					// Get the page number from the URL
+					if (isset($_GET['page'])) {
+						$pageNumber = $_GET['page'];
+					}
+
+					// Calculate the offset
+					$offset = ($pageNumber - 1) * 10;
+
+					// Get the topics for the current page
+					$sql = "SELECT * FROM topic LIMIT 10 OFFSET $offset";
+					$statement = $dbh->prepare($sql);
+					$statement->execute();
+					$currentTopics = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+					// Create the table
+					echo "<table>";
+					echo "<tr><th>Topic</th><th>User ID</th><th>Datetime</th></tr>";
+					foreach ($currentTopics as $topic) {
+						echo "<tr><td>" . $topic['Topic'] . "</td><td>" . $topic['UserID'] . "</td><td>" . $topic['DateTime'] . "</td></tr>";
+					}
+					echo "</table>";
+
+					// Create the pagination links
+					for ($i = 1; $i <= ceil($totalTopics / 10); $i++) {
+						echo "<a href='?page=$i'>$i</a> ";
+					}
+				}
+				?>
+
+			</div>
+			<div class="row" , id="footer">
+				<?php @include('Footer.php'); ?>
+			</div>
 		</div>
-	</div>
 </body>
+
 </html>
