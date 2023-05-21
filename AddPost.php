f793c70e2c84fcbc286c90ca5bad44c032fe2e7f
ayodhya.teslk@gmail.com <ayodhyaratnayake7001@gmail.com>
2023-05-21 12:59:31 +0530

    fixes

--- AddPost.php
+++ AddPost.php
@@ -5,12 +5,24 @@ include('Functions.php');
 $Post = trim($_POST['Post']);
 $Topic = trim($_GET['Topic']);
 
-$dbh = connectToDatabase();
-$statement2 = $dbh->prepare('INSERT INTO Post (UserID, Post, DateTime, TopicID) VALUES (?,?, ?, ?); ');
 date_default_timezone_set("Australia/Melbourne");
-$statement2->bindValue(1,1);
-$statement2->bindValue(2,$Post);
-$statement2->bindValue(3, date("Y/m/d H:i:s"));
-$statement2->bindValue(4,10);
-$statement2->execute();
-redirect("Forum.php?topic=$Topic");
+$dbh = connectToDatabase();
+
+$statement = $dbh->prepare('SELECT UserID FROM user WHERE username = ?');
+$statement->bindValue(1, getCookieUser());
+$statement->execute();
+$row1 = $statement->fetch(PDO::FETCH_ASSOC);
+
+$statement = $dbh->prepare('SELECT TopicID FROM Topic WHERE Topic = ?');
+$statement->bindValue(1, $Topic);
+$statement->execute();
+$row2 = $statement->fetch(PDO::FETCH_ASSOC);
+
+$statement = $dbh->prepare('INSERT INTO Post (UserID, Post, DateTime, TopicID) VALUES (?,?, ?, ?); ');
+$statement->bindValue(1,$row1['UserID']);
+$statement->bindValue(2,$Post);
+$statement->bindValue(3, date("Y/m/d H:i:s"));
+$statement->bindValue(4,$row2['TopicID']);
+$statement->execute();
+// redirect("Forum.php?topic=$Topic");
+redirect("Forum.php");
--- Forum.php
+++ Forum.php
@@ -31,13 +31,19 @@ else
 
 				// Get all of the topics from the database
 				$dbh = connectToDatabase();
-				$sql = 'SELECT * FROM topic';
-				$statement = $dbh->prepare($sql);
+				$statement = $dbh->prepare('SELECT TopicId FROM Topic WHERE Topic = ?');
+				$statement->bindValue(1,$thisTopic);
 				$statement->execute();
-				$topics = $statement->fetchAll(PDO::FETCH_ASSOC);
+				$row = $statement->fetch(PDO::FETCH_ASSOC);
+				$topicID = $row['TopicID']; 
+				// var_dump($topicID);
+				// $topics = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-				// Get the total number of topics
-				$totalTopics = count($topics);
+
+				$statement = $dbh->prepare('SELECT COUNT(Post.PostID) FROM Post WHERE Post.TopicID = ? ;');
+				$statement->bindValue(1,$topicID);
+				$statement->execute();
+				$count = $statement->fetchColumn();
 
 				// Set the default page number to 1
 				$pageNumber = 1;
@@ -50,22 +56,31 @@ else
 				// Calculate the offset
 				$offset = ($pageNumber - 1) * 10;
 
-				// Get the topics for the current page
-				$sql = "SELECT * FROM Post INNER JOIN User ON Post.UserID = User.UserID ORDER BY Post.PostID DESC LIMIT 10 OFFSET $offset";
-				$statement = $dbh->prepare($sql);
-				$statement->execute();
-				$currentPosts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+
+				// Get the topics for the current page
+				// $sql = ;
+				$statement2 = $dbh->prepare("SELECT * FROM Post INNER JOIN User ON Post.UserID = User.UserID WHERE Post.TopicID = ?  ORDER BY Post.PostID DESC LIMIT 10 OFFSET $offset;");
+				$statement2->bindValue(1, $topicID);
+				$statement2->execute();
+				$currentPosts = $statement2->fetchAll(PDO::FETCH_ASSOC);
 				// Create the table
 				echo '<div class="table-container">';
 				echo '<table class="my-table">';
-				echo '<tr><th>Topic</th><th>Created by</th><th>Datetime</th></tr>';
+				echo '<tr><th>Topic</th><th>Created by</th><th>Datetime</th><th>Likes</th></tr>';
 				foreach ($currentPosts as $post) {
 					echo '
 						<tr>
 							<td>' . $post['Post'] . '</td>
 							<td>' . $post['FirstName'] . ' ' . $post['SurName'] . '</td>
 							<td>' . $post['DateTime'] . '</td>
+							<td>
+								<span>' . $post['Likes'] . '
+									<svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M16 0.5C13.9 0.5 12.05 1.55 11 3.2C9.95 1.55 8.1 0.5 6 0.5C2.7 0.5 0 3.2 0 6.5C0 12.45 11 18.5 11 18.5C11 18.5 22 12.5 22 6.5C22 3.2 19.3 0.5 16 0.5Z" fill="#F44336"/>
+									</svg>
+								</span>
+							</td>
 						</tr>
 						';
 				}
@@ -75,8 +90,8 @@ else
 
 				// Create the pagination links
 				echo '<div class="pagination-links">';
-				for ($i = 1; $i <= ceil($totalTopics / 10); $i++) {
-					echo "<a href='?page=$i'>$i</a> ";
+				for ($i = 1; $i <= ceil($count / 10); $i++) {
+					echo '<a href="?Topic=' . urlencode($thisTopic) . '&page=' . $i . '">' . $i . '</a> ';
 				}
 				echo '</div>';
 
@@ -114,66 +129,3 @@ else
 </body>
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <?php
-		include('Functions.php');
-		$cookieMessage = getCookieMessage();
-		$cookieUser = getCookieUser();
-		?>
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="styles.css"> 
-	<meta charset="UTF-8">		
-</head>
-<body>
-	<div class="container">
-		<div class="row", id="header">
-			
-		</div>
-		<div class="row", id="nav">  
-			
-		</div>
-		<div class="row", id="content">
-			
-		</div>
-		<div class="row", id="footer">
-			
-		</div>
-	</div>
-</body>
-</html> -->
